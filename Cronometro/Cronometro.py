17cbe0d4a28b14241553769782974fbcba598cca
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2022-06-26 18:21:09 -0300

    "input"

--- Cronometro/Cronometro.py
+++ Cronometro/Cronometro.py
@@ -3,3 +3,12 @@ import time
 # Função do cronometro:
 
 def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:2d}:{:02d}' .format(mins,secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+        print("Lift off")
+
+t = input("Digite o tempo em segundos: ")
