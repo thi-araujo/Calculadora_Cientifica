e6bcc38578d2e16e2258c68d06ba3d25f6ec92d5
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2022-07-27 11:27:52 -0300

    "primeiro commit"

---
+++ Despertador/Despertador.py
@@ -0,0 +1,15 @@
+#=========================================Despertador=============================================================
+
+#_________________________________________________________________________________________________________________
+
+import datetime
+
+import os
+
+import time
+
+import random
+
+import webbrowser
+
+# Se o arquivo de URL do vídeo no Youtube não existir, crie um
