ed5dadccb935959aa062f517b6f4569272010ba0
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2022-07-28 23:07:05 -0300

    "condição if"

--- Despertador/Despertador.py
+++ Despertador/Despertador.py
@@ -13,3 +13,7 @@ import random
 import webbrowser
 
 # Se o arquivo de URL do vídeo no Youtube não existir, crie um
+
+if not os.path.isfile("youtube_alarm_videos.txt"):
+
+print('Criamdo "youtube_alarme_videos.txt"...')
