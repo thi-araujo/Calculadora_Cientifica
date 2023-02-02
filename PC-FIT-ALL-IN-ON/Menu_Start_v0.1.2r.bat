b92fac68d418a472cc521df09f297455e00084d6
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2023-02-02 07:38:00 -0300

    "Acentos e caracteres especiais"

---
+++ PC-FIT-ALL-IN-ON/Menu_Start_v0.1.2r.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cls
+
+rem Vers�o do script: 0.1.2r
+rem Data da cria��o: 18/09/2022
+rem Data da revis�o: 03/10/2022
+
+::Altera a pagina do cmd para 1252 trabalhando assim com acentos e caracteres especiais
+chcp 1252 >nul 2>&1
+
